f783dd8dce84720f60ba1148b423a6a9be1bceaf
David Barnes <dbarnes@kvcc.edu>
2024-02-25 19:52:51 -0500

    Update settings and urls to load AdminLTE2-PDQ's default stuff.

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "adminlte2_pdq",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "adminlte2_pdq.middleware.AuthMiddleware",
 ]
 
 ROOT_URLCONF = "django_project.urls"
