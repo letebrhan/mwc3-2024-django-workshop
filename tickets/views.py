530dde9d0948da3f6b74a3f8e588bfdb9d3f9635
David Barnes <dbarnes@kvcc.edu>
2024-03-03 13:35:41 -0500

    Add Open Tickets View.

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.views.generic import ListView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
 
-# Create your views here.
+from .models import Ticket
+
+
+class OpenTicketListView(TemplateView):
+    """Show lists of open tickets by priority"""
+
+    template_name = "ticket_list_open.html"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.extra_context = {
+            "open_high_tickets": Ticket.objects.filter(priority=1, completed=False),
+            "open_medium_tickets": Ticket.objects.filter(priority=2, completed=False),
+            "open_low_tickets": Ticket.objects.filter(priority=3, completed=False),
+        }
