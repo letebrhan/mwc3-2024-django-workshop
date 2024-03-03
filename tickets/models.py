e67a8babe7edc5ba2e91d476c731a54cb2925ccf
David Barnes <dbarnes@kvcc.edu>
2024-03-03 13:27:49 -0500

    Add Ticket Model.

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+# Choices for Priority
+PRIORITY_CHOICES = (
+    (1, "High"),
+    (2, "Medium"),
+    (3, "Low"),
+)
+
+
+class Ticket(models.Model):
+    """Represents a ticket for project."""
+
+    assigned_to = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        related_name="tickets",
+        blank=True,
+        null=True,
+    )
+
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    priority = models.IntegerField(choices=PRIORITY_CHOICES)
+    complete_by = models.DateField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        """str conversion"""
+        return self.name
