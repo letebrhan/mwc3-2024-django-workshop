6ffb0826830c34b76b29f89ca810afdf44a09299
David Barnes <dbarnes@kvcc.edu>
2024-03-03 14:00:31 -0500

    Add remaining URL endpoints and wire up to remaining views.

--- tickets/urls.py
+++ tickets/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 
 from .views import (
     OpenTicketListView,
+    ClosedTicketListView,
+    TicketCreateView,
+    TicketUpdateView,
+    TicketDeleteView,
 )
 
 urlpatterns = [
     path("", OpenTicketListView.as_view(), name="open_ticket_list"),
+    path("tickets/closed/", ClosedTicketListView.as_view(), name="closed_ticket_list"),
+    path("tickets/create/", TicketCreateView.as_view(), name="ticket_create"),
+    path("tickets/<int:pk>/update/", TicketUpdateView.as_view(), name="ticket_update"),
+    path("tickets/<int:pk>/delete/", TicketDeleteView.as_view(), name="ticket_delete"),
 ]
