7baf13caab9022f234a8bdd25267df8d0670fec9
David Barnes <dbarnes@kvcc.edu>
2024-03-03 13:59:49 -0500

    Finish required views for the project.

--- tickets/views.py
+++ tickets/views.py
@@ -18,3 +18,51 @@ class OpenTicketListView(TemplateView):
             "open_medium_tickets": Ticket.objects.filter(priority=2, completed=False),
             "open_low_tickets": Ticket.objects.filter(priority=3, completed=False),
         }
+
+
+class ClosedTicketListView(ListView):
+    """Show list of closed tickets"""
+
+    model = Ticket
+    template_name = "ticket_list_closed.html"
+
+    def get_queryset(self):
+        return Ticket.objects.filter(completed=True)
+
+
+class TicketCreateView(CreateView):
+    """Create a new ticket"""
+
+    model = Ticket
+    template_name = "ticket_create.html"
+    fields = "__all__"
+
+    def get_success_url(self):
+        """Where to send user on success"""
+        messages.success(self.request, "Ticket Created Successfully")
+        return reverse("open_ticket_list")
+
+
+class TicketUpdateView(UpdateView):
+    """Update a ticket"""
+
+    model = Ticket
+    template_name = "ticket_update.html"
+    fields = "__all__"
+
+    def get_success_url(self):
+        """Where to send user on success"""
+        messages.success(self.request, "Ticket Updated Successfully")
+        return reverse("open_ticket_list")
+
+
+class TicketDeleteView(DeleteView):
+    """Delete a ticket"""
+
+    model = Ticket
+    template_name = "ticket_delete.html"
+
+    def get_success_url(self):
+        """Where to send user on success"""
+        messages.success(self.request, "Ticket Deleted Successfully")
+        return reverse("open_ticket_list")
